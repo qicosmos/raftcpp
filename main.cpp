b2ab0e740d4d1ec1cf786717d384403aa70fa1ad
Gitea <gitea@fake.local>
2019-04-23 10:43:42 +0800

    update for gcc

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <filesystem>
+#include <string_view>
 #include "raft_server.hpp"
 using namespace raftcpp;
 
 std::pair<bool, config> get_conf(std::string_view path) {
-	std::ifstream ifile(path, std::ios::binary);
+	std::ifstream ifile(path.data(), std::ios::binary);
 	if (!ifile) {
 		return {};
 	}
 
-	auto size = std::filesystem::file_size(path);
+	auto size = std::filesystem::file_size(path.data());
 	std::string str;
 	str.resize(size);
 	ifile.read(&str[0], size);
