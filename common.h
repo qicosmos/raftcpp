4a13778a00482d89ac2e2f59ff39a01d6bed7f90
qicosmos <qicosmos@163.com>
2019-06-04 19:43:31 +0800

    update

--- common.h
+++ common.h
@@ -3,7 +3,7 @@
 #include <vector>
 #include <any>
 #include <atomic>
-#include <iguana/json.hpp>
+//#include <iguana/json.hpp>
 #include "entity.h"
 
 namespace raftcpp {
@@ -12,12 +12,12 @@ namespace raftcpp {
 		int port;
 		int host_id;
 	};
-	REFLECTION(address, ip, port, host_id);
+	//REFLECTION(address, ip, port, host_id);
 
 	struct config {
 		std::vector<address> peers_addr;
 	};
-	REFLECTION(config, peers_addr);
+	//REFLECTION(config, peers_addr);
 
 	inline constexpr int ELECTION_TIMEOUT = 500;//milliseconds
 	inline constexpr int VOTE_TIMEOUT = 500;//milliseconds
